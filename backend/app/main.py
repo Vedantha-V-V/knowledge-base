96fa6bd6abdb1908ad320b17f66e88e1c6a1db4f
Vedantha-V-V <vernekar.vedantha2004@gmail.com>
2026-05-04 11:18:24 +0530

    Updated Fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -134,7 +134,7 @@ def health():
 
 @app.get("/")
 def root():
-    return {"message": "CI/CD running"}
+    return {"message": "CI/CD is running"}
 
 # -------------------- QUERY --------------------
 @app.post("/query", response_model=QueryResponse)
